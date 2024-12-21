b31a7754553bc2f470a4e4199b5c9f5224adffbd
lushuo <ls13903711991@sina.com>
2024-12-21 23:12:20 +0800

    初步的GUI

---
+++ Assets/Scripts/UI/GUIManager.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUIManager : MonoSingleton<GUIManager>
+{
+    public List<GameObject> panels; //面板资源
+
+    public Stack<GameObject> panelStack = new Stack<GameObject>(); //通过栈来管理各个UI面板的显示顺序
+
+    public int currentNumber;
+
+    public GameObject currentPanelObj
+    {
+        get
+        {
+            return panelStack.Peek();
+        }
+    }
+
+    private void OnEnable()
+    {
+        OpenPanel(currentNumber);
+    }
+
+    public void OpenPanel(int id, bool isHidePrevious = false)
+    {
+        if (isHidePrevious)
+        {
+            if (panelStack.Peek() != null)  //可以出栈
+            {
+                currentPanelObj.SetActive(false);
+                panelStack.Pop();
+            }
+            panelStack.Push(panels[id]); //入栈
+            currentPanelObj.SetActive(true);
+
+        }
+        else
+        {
+            panelStack.Push(panels[id]);
+            currentPanelObj.SetActive(true);
+        }
+    }
+    //返回前一页
+    public void Back()
+    {
+        if (panelStack.Peek() != null)
+        {
+            currentPanelObj.SetActive(false);
+            panelStack.Pop();
+        }
+        currentPanelObj.SetActive(true);
+
+    }
+}
---
+++ Assets/Scripts/UI/MonoSingleton.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MonoSingleton<T> : MonoBehaviour   //泛型约束，类为抽象类，只让子类实例化
+    where T :MonoBehaviour    //T继承
+{
+    static T m_instance;
+
+    public static T Instance { get => m_instance;}
+
+    protected virtual void Awake()
+    {
+        m_instance = this as T;
+    }
+}
